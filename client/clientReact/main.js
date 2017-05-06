433b67ae68cf9e346bd651f8e0a25db372f13176
Naomi Mine <naomi.mine@gmail.com>
2017-05-06 13:59:39 -0500

    first input component

--- client/clientReact/main.js
+++ client/clientReact/main.js
@@ -4,15 +4,54 @@ var request = require('superagent');
 
 var MainComponent = React.createClass({
 	getInitialState: function(){
-		return{}
+		return{roommateName: ''}
+	},
+	getNameInfo: function(roommateName){
+		console.log(roommateName);
+		var state = this.state;
+		state.roommateName = roommateName;
+		this.setState(state);
 	},
 	render: function(){
 		return (
 			<div>
-				<h1>This is react page</h1>
+				<InputComponent getNameInfo={this.getNameInfo} />
 			</div>
 		)
 	}
 })
+
+var InputComponent = React.createClass({
+	getInitialState: function(){
+		return {roommateName: ''}
+	},
+	handleNameChangeInput: function(e){
+		var state = this.state;
+		state.roommateName = e.target.value;
+		this.setState(state);
+		console.log("this is roommate " + this.state.roommateName)
+		console.log(this.state)
+	},
+	handleNameEntry: function(){
+		console.log("clickkkk");
+		console.log("these are props", this.props);
+		console.log("This is the state: " + this.state.roommateName)
+		this.props.getNameInfo(this.state.roommateName);
+		console.log("roommate name is " + this.state.roommateName);
+	},
+	render: function(){
+		return (
+			<div>
+				<h1>Input component</h1>
+				<div>
+					<input onChange={this.handleNameChangeInput} name="roommateName" type="text" value={this.state.roommateName}/>
+					<div><button onClick={this.handleNameEntry}> Log :) </button></div>
+				</div>
+			</div>
+
+		)
+	}
+})
+
 
 ReactDOM.render(<MainComponent/>, document.getElementById('container'));
--- server/public/js/build/build.js
+++ server/public/js/build/build.js
@@ -7,7 +7,42 @@ var MainComponent = React.createClass({
 	displayName: 'MainComponent',
 
 	getInitialState: function () {
-		return {};
+		return { roommateName: '' };
+	},
+	getNameInfo: function (roommateName) {
+		console.log(roommateName);
+		var state = this.state;
+		state.roommateName = roommateName;
+		this.setState(state);
+	},
+	render: function () {
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(InputComponent, { getNameInfo: this.getNameInfo })
+		);
+	}
+});
+
+var InputComponent = React.createClass({
+	displayName: 'InputComponent',
+
+	getInitialState: function () {
+		return { roommateName: '' };
+	},
+	handleNameChangeInput: function (e) {
+		var state = this.state;
+		state.roommateName = e.target.value;
+		this.setState(state);
+		console.log("this is roommate " + this.state.roommateName);
+		console.log(this.state);
+	},
+	handleNameEntry: function () {
+		console.log("clickkkk");
+		console.log("these are props", this.props);
+		console.log("This is the state: " + this.state.roommateName);
+		this.props.getNameInfo(this.state.roommateName);
+		console.log("roommate name is " + this.state.roommateName);
 	},
 	render: function () {
 		return React.createElement(
@@ -16,7 +51,21 @@ var MainComponent = React.createClass({
 			React.createElement(
 				'h1',
 				null,
-				'This is react page'
+				'Input component'
+			),
+			React.createElement(
+				'div',
+				null,
+				React.createElement('input', { onChange: this.handleNameChangeInput, name: 'roommateName', type: 'text', value: this.state.roommateName }),
+				React.createElement(
+					'div',
+					null,
+					React.createElement(
+						'button',
+						{ onClick: this.handleNameEntry },
+						' Log :) '
+					)
+				)
 			)
 		);
 	}
