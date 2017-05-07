527a638a8c8135865f8cbf5556798728c1f05714
Naomi Mine <naomi.mine@gmail.com>
2017-05-07 16:26:26 -0500

    working on preventing initial rerender

--- client/clientReact/main.js
+++ client/clientReact/main.js
@@ -3,58 +3,76 @@ var ReactDOM = require('react-dom');
 var request = require('superagent');
 // var Select = require('react-select');
 
-class MainComponent extends React.Component {
-	render() {
-		return (
-			<div>
-				<InputComponent />
-			</div>
-		)
-	}
-}
 
 class InputComponent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			roommatename: ''
+			roommatename: '',
+			chorename: '',
+			entryInfo: []
 		}
+
 		this.handleNameInput = this.handleNameInput.bind(this)
 		this.handleSelectChange = this.handleSelectChange.bind(this);
 		this.handleDataEntry = this.handleDataEntry.bind(this);
 	}
 
+	componentDidMount(){
+		var state = this.state;
+		var self = this;
+		request.get('http://localhost:9292/home/chores')
+		.end(function(err, data){
+			state.entryInfo = data.body;
+			self.setState(state);
+			console.log(state, " this is the state in componentdidmount")
+		})
+	}
 	handleNameInput(e){
 		this.setState({roommatename: e.target.value});
 	}
 	handleSelectChange(e){
-		this.setState({value: e.target.value});
+		this.setState({chorename: e.target.value});
 	}
 	handleDataEntry(e){
 		e.preventDefault();
-		console.log(this.state)
+		console.log("state object: ", this.state)
 	}
 	render(){
+		// console.log(this.state.entryInfo, "render state")
+		// let entries = this.state.entryInfo
+		// console.log(entries, "these are entries")
+		console.log(this.state, "this is state")
+
+		// let choreHistory = entries.map((entry, i) =>
+		// 	<li key={i}>
+		// 		{entry.roommatename}
+		// 	</li>
+		// )
+
+		return (
+
+			<div>
+				<NewComponent handleDataEntry={this.handleDataEntry} handleNameInput={this.handleNameInput} handleSelectChange={this.handleSelectChange}/>
+			</div>
+
+		)
+	}
+}
+class NewComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			roommatename: '',
+			chorename: '',
+			entryInfo: []
+		}
+	}
+	render() {
 		return (
 			<div className="inputComponentDiv">
-				<div>
-					<form onSubmit={this.handleDataEntry.bind(this)}>
-						<div>
-							<input type="text" value={this.state.roommatename} onChange={this.handleNameInput.bind(this)}/>
-						</div>
-						<div>
-							<select value={this.state.value} onChange={this.handleSelectChange}>
-								<option value="litter">cleaned litter box</option>
-								<option value="foodPurchase">bought food</option>
-								<option value="nails">trimmed nails</option>
-								<option value="feed">fed the kitty</option>
-							</select>
-						</div>
-						<div>
-							<button type="submit"> Log :) </button>
-						</div>
-					</form>
-				</div>
+				<h1>yo</h1>
+
 			</div>
 
 		)
@@ -62,4 +80,52 @@ class InputComponent extends React.Component {
 }
 
 
-ReactDOM.render(<MainComponent/>, document.getElementById('container'));
+// <form onSubmit={this.handleDataEntry.bind(this)}>
+// 	<div>
+// 		<input type="text" value={this.state.roommatename} onChange={this.handleNameInput.bind(this)}/>
+// 	</div>
+// 	<div>
+// 		<select value={this.state.value} onChange={this.handleSelectChange}>
+// 			<option value="litter">cleaned litter box</option>
+// 			<option value="foodPurchase">bought food</option>
+// 			<option value="nails">trimmed nails</option>
+// 			<option value="feed">fed the kitty</option>
+// 		</select>
+// 	</div>
+// 	<div>
+// 		<button type="submit"> Log :) </button>
+// 	</div>
+// </form>
+
+
+//
+// class ListComponent extends React.Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = {
+// 			entryInfo: []
+// 		}
+// 		console.log("ugh state ", this.state)
+//
+// 	}
+// 	render(){
+// 		return (
+// 			<h1>list component</h1>
+// 		)
+// 	}
+// }
+
+// function () {
+// 		const entries = this.state.entryInfo
+// 		console.log(entries, "these are entries")
+// 		// const listItems = roommates.map((roommate, ) =>
+// 		// <li key={roommates.id}></li>
+// 		// )
+// 		return (
+//
+// 		);
+
+// }
+
+
+ReactDOM.render(<InputComponent/>, document.getElementById('container'));
--- server/public/js/build/build.js
+++ server/public/js/build/build.js
@@ -4,96 +4,130 @@ var ReactDOM = require('react-dom');
 var request = require('superagent');
 // var Select = require('react-select');
 
-class MainComponent extends React.Component {
-	render() {
-		return React.createElement(
-			'div',
-			null,
-			React.createElement(InputComponent, null)
-		);
-	}
-}
 
 class InputComponent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			roommatename: ''
+			roommatename: '',
+			chorename: '',
+			entryInfo: []
 		};
+
 		this.handleNameInput = this.handleNameInput.bind(this);
 		this.handleSelectChange = this.handleSelectChange.bind(this);
 		this.handleDataEntry = this.handleDataEntry.bind(this);
 	}
 
+	componentDidMount() {
+		var state = this.state;
+		var self = this;
+		request.get('http://localhost:9292/home/chores').end(function (err, data) {
+			state.entryInfo = data.body;
+			self.setState(state);
+			console.log(state, " this is the state in componentdidmount");
+		});
+	}
 	handleNameInput(e) {
 		this.setState({ roommatename: e.target.value });
 	}
 	handleSelectChange(e) {
-		this.setState({ value: e.target.value });
+		this.setState({ chorename: e.target.value });
 	}
 	handleDataEntry(e) {
 		e.preventDefault();
-		console.log(this.state);
+		console.log("state object: ", this.state);
+	}
+	render() {
+		// console.log(this.state.entryInfo, "render state")
+		// let entries = this.state.entryInfo
+		// console.log(entries, "these are entries")
+		console.log(this.state, "this is state");
+
+		// let choreHistory = entries.map((entry, i) =>
+		// 	<li key={i}>
+		// 		{entry.roommatename}
+		// 	</li>
+		// )
+
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(NewComponent, { handleDataEntry: this.handleDataEntry, handleNameInput: this.handleNameInput, handleSelectChange: this.handleSelectChange })
+		);
+	}
+}
+class NewComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			roommatename: '',
+			chorename: '',
+			entryInfo: []
+		};
 	}
 	render() {
 		return React.createElement(
 			'div',
 			{ className: 'inputComponentDiv' },
 			React.createElement(
-				'div',
+				'h1',
 				null,
-				React.createElement(
-					'form',
-					{ onSubmit: this.handleDataEntry.bind(this) },
-					React.createElement(
-						'div',
-						null,
-						React.createElement('input', { type: 'text', value: this.state.roommatename, onChange: this.handleNameInput.bind(this) })
-					),
-					React.createElement(
-						'div',
-						null,
-						React.createElement(
-							'select',
-							{ value: this.state.value, onChange: this.handleSelectChange },
-							React.createElement(
-								'option',
-								{ value: 'litter' },
-								'cleaned litter box'
-							),
-							React.createElement(
-								'option',
-								{ value: 'foodPurchase' },
-								'bought food'
-							),
-							React.createElement(
-								'option',
-								{ value: 'nails' },
-								'trimmed nails'
-							),
-							React.createElement(
-								'option',
-								{ value: 'feed' },
-								'fed the kitty'
-							)
-						)
-					),
-					React.createElement(
-						'div',
-						null,
-						React.createElement(
-							'button',
-							{ type: 'submit' },
-							' Log :) '
-						)
-					)
-				)
+				'yo'
 			)
 		);
 	}
 }
 
-ReactDOM.render(React.createElement(MainComponent, null), document.getElementById('container'));
+// <form onSubmit={this.handleDataEntry.bind(this)}>
+// 	<div>
+// 		<input type="text" value={this.state.roommatename} onChange={this.handleNameInput.bind(this)}/>
+// 	</div>
+// 	<div>
+// 		<select value={this.state.value} onChange={this.handleSelectChange}>
+// 			<option value="litter">cleaned litter box</option>
+// 			<option value="foodPurchase">bought food</option>
+// 			<option value="nails">trimmed nails</option>
+// 			<option value="feed">fed the kitty</option>
+// 		</select>
+// 	</div>
+// 	<div>
+// 		<button type="submit"> Log :) </button>
+// 	</div>
+// </form>
+
+
+//
+// class ListComponent extends React.Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = {
+// 			entryInfo: []
+// 		}
+// 		console.log("ugh state ", this.state)
+//
+// 	}
+// 	render(){
+// 		return (
+// 			<h1>list component</h1>
+// 		)
+// 	}
+// }
+
+// function () {
+// 		const entries = this.state.entryInfo
+// 		console.log(entries, "these are entries")
+// 		// const listItems = roommates.map((roommate, ) =>
+// 		// <li key={roommates.id}></li>
+// 		// )
+// 		return (
+//
+// 		);
+
+// }
+
+
+ReactDOM.render(React.createElement(InputComponent, null), document.getElementById('container'));
 
 },{"react":184,"react-dom":31,"superagent":185}],2:[function(require,module,exports){
 
